68e19c54b9a692f8a1cb482c8dfb821e7894d5e6
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2020-11-06 15:54:29 +0100

    Rework of all the database mapping

---
+++ api/src/main/java/com/finalproject/sheeks/entities/Jeux.java
@@ -0,0 +1,2 @@
+package com.finalproject.sheeks.entities;public class Jeux {
+}
--- api/src/main/java/com/finalproject/sheeks/repositories/RoleRepository.java
+++ api/src/main/java/com/finalproject/sheeks/repositories/RoleRepository.java
@@ -1,6 +1,5 @@
 package com.finalproject.sheeks.repositories;
 
-import com.finalproject.sheeks.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, String> {
